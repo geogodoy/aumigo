20b56da553c96a7d18301f385c020df37b626ec3
geogodoy <geovana.godoy12@gmail.com>
2024-12-05 17:43:24 -0300

    criado projeto aumigo

---
+++ src/main/java/com/geogodoy/aumigo/AumigoApplication.java
@@ -0,0 +1,13 @@
+package com.geogodoy.aumigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AumigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AumigoApplication.class, args);
+	}
+
+}
